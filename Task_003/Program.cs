6be2984769a2eaddc38e8183927fd497a639abfe
AlexseyLoginov <devileean@gmail.com>
2022-11-10 23:31:12 +0300

    task 3 complete

---
+++ Task_003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести на экран число «ПИ» с точностью до сотых. (и т.д.)
+
+Console.WriteLine("{0: 0.0}", Math.PI);
+Console.WriteLine("{0: 0.00}", Math.PI);
+Console.WriteLine("{0: 0.000}", Math.PI);
+Console.WriteLine("{0: 0.0000}", Math.PI);
+Console.WriteLine("{0: 0.00000}", Math.PI);
+Console.WriteLine("{0: 0.000000}", Math.PI);
+Console.WriteLine("{0: 0.0000000}", Math.PI);
