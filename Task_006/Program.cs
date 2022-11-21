d66382f3fad067eecabea3adc3c216a0ff71c172
AlexseyLoginov <devileean@gmail.com>
2022-11-22 00:48:10 +0300

    task 006 calculate their sum, difference, product and quotient

---
+++ Task_006/Program.cs
@@ -0,0 +1,14 @@
+﻿// Даны два действительных числа x и у.
+// Вычислить их сумму, разность, произведение и частное.
+
+Console.Write("Введите число X: ");
+var x = Convert.ToInt64(Console.ReadLine());
+Console.Write("Введите число Y: ");
+var y = Convert.ToInt64(Console.ReadLine());
+
+var amount = x + y;
+var difference = x - y;
+var quotient = x * y;
+var product = x / y;
+    
+Console.WriteLine($"сумма = {amount}, разность = {difference}, произведение = {quotient} и частное = {product}");
