8fbbb69d64047464952e714853cec0d6e62edc26
AlexseyLoginov <devileean@gmail.com>
2022-11-22 00:15:07 +0300

    task 005 returns the natural logarithm of e

---
+++ Task_005/Program.cs
@@ -0,0 +1,9 @@
+﻿// 5. Задача.
+// Вывести на экран число e (основание натурального логарифма) с точностью до десятых.
+
+Console.WriteLine("Введите число: ");
+var num = Convert.ToDouble(Console.ReadLine());
+
+var e = (double) Math.Log(num);
+
+Console.WriteLine("{0: 0.00}" , e);
